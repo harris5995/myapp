7f4805a9af469e9afffb4359a58d24fef8c795a0
Harris Dzulkarnain <harisidzwan@gmail.com>
2023-08-24 16:24:48 +0800

    Workable Project, Alerts are still colorless

--- src/lib/Alert.svelte
+++ src/lib/Alert.svelte
@@ -4,21 +4,28 @@
 
   let alertType = null
   let alertMessage = null
+  let alertBackgroundColor = ""; // Initialize the background color variable
 
-  // setting our local alertType and alertMessage whenever alerts.set Alert is called
+  // setting our local alertType and alertMessage whenever alerts.setAlert is called
   $: if ($alerts.type && $alerts.message) {
       alertType = $alerts.type
       alertMessage = $alerts.message
+      resetAlerts(); // Reset the timer whenever a new alert is set
   }
 
   // reset
   let clearingAlert = false;
+  let clearAlertTimeout; // To store the timeout reference
 
   function resetAlerts() {
     if (!clearingAlert && alertMessage) {
       clearingAlert = true;
 
-      setTimeout(() => {
+      if (clearAlertTimeout) {
+        clearTimeout(clearAlertTimeout); // Clear previous timeout if exists
+      }
+
+      clearAlertTimeout = setTimeout(() => {
         alertType = null;
         alertMessage = null;
         clearingAlert = false;
@@ -26,13 +33,10 @@
     }
   }
 
-
-  //Alert stops when webpage is changed
+  // Alert stops when webpage is changed
   afterNavigate(() => resetAlerts())
-
+  
   // Set alert color class based on alert type
-  let alertBackgroundColor = ""; // Initialize the background color variable
-
   if (alertType === ALERT_TYPES.SUCCESS) {
     alertBackgroundColor = "bg-green-500"; // Green color class for success
   } else if (alertType === ALERT_TYPES.WARNING) {
@@ -43,12 +47,13 @@
 
 </script>
 
-
 {#if alertMessage}
-  <div class="fixed top-[110px] inset-x-0.5 z-10 container shadow-lg max-w-sm mx-auto rounded-box flex justify-center" style="background-color: {alertBackgroundColor}">
-    <div class="flex justify-center">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="container stroke-current flex-shrink-0 w-5 h-5"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
-      <span>{alertMessage}</span>
+  <div class="fixed top-[80px] inset-x-0.5 z-10 container shadow-lg max-w-sm mx-auto rounded-box flex justify-center" style="background-color: {alertBackgroundColor}">
+    <div class="flex items-center justify-center p-4 space-x-4">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="w-6 h-6 stroke-current text-white">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+      </svg>
+      <span class="text-white">{alertMessage}</span>
     </div>
   </div>
 {/if}
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,30 @@
 <!-- Contains the stuff that appears on all webpages -->
 
 <script>
-    import "../app.css";
-    import Navbar from "../lib/Navbar.svelte";
-    import Alert from "../lib/Alert.svelte";
+  import "../app.css";
+  import Navbar from "../lib/Navbar.svelte";
+  import Alert from "../lib/Alert.svelte";
 </script>
 
 <style>
-    main {
-      padding-top: 4rem; /* Increase this value as needed */
-    }
+  main {
+    padding-top: 4rem; /* Increase this value as needed */
+  }
 
-     /* Add margin-top to Alert to move it down */
-     .alert-container {
-      margin-top: 2rem; /* Adjust this value to move the Alert down */
-    }
+   /* Add margin-top to Alert to move it down */
+   .alert-container {
+    margin-top: 2rem; /* Adjust this value to move the Alert down */
+  }
 
 </style>
 
 <Navbar/>
 
 <div class="alert-container">
-    <Alert/>
-  </div>
+  <Alert/>
+</div>
 
 
 <main>
-  <slot />
+<slot />
 </main>
